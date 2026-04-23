e021a11ebe7d56a55f23574a074c284f915565cd
mumin ahmed <momin1aden2@gmail.com>
2026-04-23 23:00:28 +0800

    chore: bump sw cache to force apphosting rollout picking up new env vars

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,6 +1,6 @@
 // Service worker: network-first HTML, cache-first assets, network-only APIs.
 
-var CACHE_NAME = 'renteriq-shell-v138';
+var CACHE_NAME = 'renteriq-shell-v139';
 
 var APP_SHELL = [
   '/app/index.html',
